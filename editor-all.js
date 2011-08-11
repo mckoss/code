a12ce67c5ded1a8c99f86d171c80f1d7d27b4aec
Mike Koss <mckoss@startpad.org>
2011-08-10 18:14:29 -0700

    simple logging output

--- editor-all.js
+++ editor-all.js
@@ -3753,7 +3753,9 @@ var scope;
 
 exports.extend({
     'init': init,
-    'log': log
+    'log': log,
+    'getUsers': getUsers,
+    'getUserLog': getUserLog
 });
 
 function init(_username, _storage, _scope) {
@@ -3767,6 +3769,22 @@ function log(eventName, data) {
     var obj = types.extend(data, {event: eventName, scope: scope, time: new Date().toString()});
     console.log("Logging: " + JSON.stringify(obj));
     storage.push(LOG_DOC, logAs, obj);
+}
+
+function getUsers(callback) {
+    var users = [];
+    storage.list(LOG_DOC, undefined, undefined, function (json) {
+        for (var prop in json.items) {
+            users.push(prop);
+        }
+        callback(users);
+    });
+}
+
+function getUserLog(username, callback) {
+    storage.getBlob(LOG_DOC, username, undefined, function (data) {
+        callback(username, data);
+    });
 }
 });
 
